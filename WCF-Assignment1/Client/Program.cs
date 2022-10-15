064339cf8f4d0c2c9e7d60c2c82065866208ea8b
Devon Tomlin <devonalantomlin1@gmail.com>
2022-10-15 14:29:12 -0400

    Initial project structure

---
+++ WCF-Assignment1/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        // After setting console as startup client, we need to link it to the WCF service
+        // 
+        static void Main(string[] args)
+        {
+            ServiceReference1.ServiceClient client = new ServiceReference1.ServiceClient();
+            var response = client.GetData(100);
+            Console.WriteLine(response);
+            Console.ReadLine();
+        }
+    }
+}
